3c03c7ec2d1ca84e387f6f4dfdd75bb583976306
Andr Pi <andr.podo@gmail.com>
2025-03-23 17:06:05 +0200

    hw18.2

--- hw18.cy.js
+++ hw18.cy.js
@@ -1,10 +1,21 @@
 
-describe('example to-do app', () => {
+describe('authorize on site', () => {
   beforeEach(() => {
     cy.visit('https://qauto.forstudy.space', {auth:{username: "guest", password: "welcome2qauto"} })
   })
 
-  it('My first test', () => {
-    cy.get('.todo-list li').should('have.length', 2)
-  })
+  it('Find all button on header', () => {
+    cy.get('button').should('have.length.greaterThan', 0);
+  });
+  it('should find a button by its exact text', () => {
+    // Знаходимо кнопку з конкретним текстом
+    cy.contains('button', 'Sign up').should('exist');
+  });
+  it('should find all links in the footer', () => {
+    // Перевіряємо, чи є посилання у футері
+    cy.get('footer a').should('have.length.greaterThan', 0).each((link) => {
+        cy.wrap(link).should('have.attr', 'href'); // Переконуємось, що у посилань є атрибут href
+    });
+  });
+
 })
